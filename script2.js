9c804cff6ec1c0453bd1ddfe29e382a7a4ce6d44
Abhishek Gogoi <44311710+AbhishekGogoi@users.noreply.github.com>
2024-02-20 15:33:52 +0530

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,19 @@
+const isStrongPassword = (password) => {
+
+    
+    if(password.length < 8){
+        return false;
+    }
+    
+    if(password.toLowerCase().includes("password")){
+        return false;
+    }
+    
+    if(!/[A-Z]/.test(password)){
+        return false;
+    }
+    
+    return true;
+}
+
+console.log(isStrongPassword("Qwerty"));
